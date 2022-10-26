c05959eb0ca2e4b40208c57fa2973a4482ef2320
koss054 <konstantin054@abv.bg>
2022-10-26 18:04:41 +0300

    Add user controller

---
+++ Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookList.Controllers
+{
+    public class UserController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
