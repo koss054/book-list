a9e323f324a35264e43c56b47207a91923ca5d74
koss054 <konstantin054@abv.bg>
2022-10-26 17:50:53 +0300

    Configure OnModelCreating in the context

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,13 +1,35 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace BookList.Data
+﻿namespace BookList.Data
 {
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    using Data.Entities;
+
+    using static Constants.DataConstants.User;
+
     public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<UserBook>()
+                .HasKey(ub => new { ub.UserId, ub.BookId });
+
+            builder.Entity<User>()
+                .Property(u => u.UserName)
+                .HasMaxLength(MaxUserNameLength)
+                .IsRequired();
+
+            builder.Entity<User>()
+                .Property(u => u.Email)
+                .HasMaxLength(MaxEmailLength)
+                .IsRequired();
+
+            base.OnModelCreating(builder);
         }
     }
 }
