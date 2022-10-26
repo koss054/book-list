97ca2b5dab6511db74578efdc02dfe930b88ec2e
koss054 <konstantin054@abv.bg>
2022-10-26 18:03:58 +0300

    Add base controller

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,14 @@
+﻿namespace BookList.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize]
+    public class BaseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
